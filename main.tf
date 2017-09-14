fa016567d270d4d4ca61d30e5cd6f50a8b97a916
Arturo Pie <arturop@nulogy.com>
2017-09-14 18:03:26 -0400

    deploy a 'hello world' server

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ provider "aws" {
 data "aws_ami" "ubuntu" {
   filter {
     name = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-trusty-14.04-amd64-server-20160217.1"]
+    values = ["ubuntu/images/hvm-ssd/ubuntu-xenial-16.04-amd64-server-20161020"]
   }
 
   owners = ["099720109477"]
@@ -15,7 +15,29 @@ resource "aws_instance" "my-instance" {
   ami = "${data.aws_ami.ubuntu.id}"
   instance_type = "t2.micro"
 
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, World" > index.html
+              nohup busybox httpd -f -p 8080 &
+              EOF
+
+  vpc_security_group_ids = ["${aws_security_group.my-sg.id}"]
   tags {
     Name = "Hello World"
   }
+}
+
+resource "aws_security_group" "my-sg" {
+  name = "example security group"
+
+  ingress {
+    from_port = 8080
+    protocol = "tcp"
+    to_port = 8080
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+output "public_url" {
+  value = "${aws_instance.my-instance.public_ip}:8080"
 }
