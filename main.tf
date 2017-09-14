63fd3248c92eceb78ed3a666faf0066a7e6a66cc
Arturo Pie <arturop@nulogy.com>
2017-09-14 17:38:09 -0400

    adds data aws_ami and interpolation

--- main.tf
+++ main.tf
@@ -2,8 +2,17 @@ provider "aws" {
   region = "us-west-2"
 }
 
+data "aws_ami" "ubuntu" {
+  filter {
+    name = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-trusty-14.04-amd64-server-20160217.1"]
+  }
+
+  owners = ["099720109477"]
+}
+
 resource "aws_instance" "my-instance" {
-  ami = "ami-aa5ebdd2"
+  ami = "${data.aws_ami.ubuntu.id}"
   instance_type = "t2.micro"
 
   tags {
